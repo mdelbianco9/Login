eea78e852c97cd4ca6c31c6acb83a9c1c290798d
marcus delbianco <mdelbianco9@gmail.com>
2017-07-30 00:03:40 -0700

    login works

--- js/main.js
+++ js/main.js
@@ -24,27 +24,30 @@ function login() {
 		}
 };
 
+// This function adds a set of data to the array
 function register() {
 	var regusername = document.getElementById('regname').value
 	var regpassword = document.getElementById('regpw').value
+	// box for new username and password to be added
 	var newuser = {
 		username: regusername,
 		password: regpassword
 	}
+	// itterates through the array
 	for (var i = 0; i < userdata.length; i++) {
+		// checks to see if username is already taken
 		if (regusername == userdata[i].username) {
 			alert('That username is already taken');
 			return
+			// check to see if password is long enough
 		} else if (regpassword.length < 8) {
 			alert('Your password is too short, make it 8 or more characters');
 			return
-		} else {
-			alert("You Are All Signed Up");
-	
-		}
+			// is suppose to add the new username and pw to the array
+		} 
+
 	}
-
-	userdata.push(newuser);
 	console.log(userdata);
-
+	alert("You Are Registered! Please log in");
+	userdata.push(newuser);
 };
